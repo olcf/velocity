8fcda4784249ac3e0a5124ff44d38825e3829815
Asa Rentschler <rentschleraj@ornl.gov>
2024-12-30 15:01:30 -0500

    Ignored a few lint rules in __init__

--- src/velocity/__init__.py
+++ src/velocity/__init__.py
@@ -1,5 +1,5 @@
-from loguru import logger; logger.disable("velocity")  # disable logging at the module level
-from ._config import config
+from loguru import logger; logger.disable("velocity")   # noqa: E702 # disable logging at the module level
+from ._config import config # noqa: E402
 
 
 __all__ = [config]
